d87772fcb50828f0d623888b49a89e30e362d979
NicoleLund <78176837+NicoleLund@users.noreply.github.com>
2021-06-17 11:00:57 -0700

    Display datetime filtered data

--- b_UFO_level_1/static/js/app.js
+++ b_UFO_level_1/static/js/app.js
@@ -1,8 +1,24 @@
 // from data.js
 var tableData = data;
 
-// reference to the table body
-var tbody = d3.select("tbody");
+// reference to the table elements
+var table = d3.select("#ufo-table");
+
+// Add data to table body
+function loadTable(data) {
+    var tbody = d3.select("tbody");
+    data.forEach((sighting) => {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
+
+// Load full data table
+loadTable(tableData);
+
 
 // reference to the filter input
 var dateFilter = d3.select("#form");
@@ -14,19 +30,27 @@ var filterButton = d3.select("#filter-btn")
 dateFilter.on("submit", filterData);
 filterButton.on("click", filterData);
 
-// Add data to table body
-tableData.forEach((sighting) => {
-    var row = tbody.append("tr");
-    Object.entries(sighting).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
-    });
-});
-
 // Filter data table
 function filterData() {
+    
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Get datetime filter input
     var filterText = d3.select("#datetime").property("value");
     console.log(filterText);
+
+    // Filter tableData on datetime
     var filteredData = tableData.filter(sighting => sighting.datetime === filterText);
     console.log(filteredData);
+
+    // remove existing table data
+    d3.select("tbody").remove();
+
+    // add table body back
+    table.append("tbody");
+
+    // load table
+    loadTable(filteredData);
+
 };
