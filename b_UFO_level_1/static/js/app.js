e1941852502678a5c01d4323a5364d13a348cf48
NicoleLund <78176837+NicoleLund@users.noreply.github.com>
2021-06-17 10:01:54 -0700

    Build table body

--- b_UFO_level_1/static/js/app.js
+++ b_UFO_level_1/static/js/app.js
@@ -1,4 +1,14 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// reference the table body
+var tbody = d3.select("tbody");
+
+// Add data to table body
+tableData.forEach((sighting) => {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
